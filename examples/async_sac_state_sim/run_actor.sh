7e23bbd16d9bd4a887f643852784ba2a63da8d60
Juan Rojas <rojas70@gmail.com>
2025-04-10 16:59:28 -0500

    Pick and Place bash script corrections

--- examples/async_sac_state_sim/run_actor.sh
+++ examples/async_sac_state_sim/run_actor.sh
@@ -7,4 +7,12 @@ python async_sac_state_sim.py "$@" \
     --exp_name=serl_dev_sim_test \
     --seed 0 \
     --random_steps 1000 \
-    --debug
+    --max_steps 1000000 \
+    --training_starts 1000 \
+    --critic_actor_ratio 8 \
+    --batch_size 256 \
+    --save_model True \
+    --checkpoint_period 10000 \
+    --checkpoint_path "/home/student/robot/robot_ws/src/serl/examples/async_sac_state_sim/checkpoints" \ 
+    #--debug # wandb is disabled when debug
+
--- examples/async_sac_state_sim/run_learner.sh
+++ examples/async_sac_state_sim/run_learner.sh
@@ -5,7 +5,11 @@ python async_sac_state_sim.py "$@" \
     --env PandaPickCube-v0 \
     --exp_name=serl_dev_sim_test \
     --seed 0 \
+    --max_steps 1000000 \
     --training_starts 1000 \
     --critic_actor_ratio 8 \
     --batch_size 256 \
-    --debug # wandb is disabled when debug
+    --save_model True \
+    --checkpoint_period 10000 \
+    --checkpoint_path "/home/student/robot/robot_ws/src/serl/examples/async_sac_state_sim/checkpoints" \ 
+    #--debug # wandb is disabled when debug
