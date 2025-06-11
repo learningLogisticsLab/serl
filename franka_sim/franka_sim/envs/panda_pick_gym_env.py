422d207e717c6ce13bc9106327daf5cf0b6ec456
bison <rojas70@gmail.com>
2025-06-11 16:46:55 -0500

    Fix bug in the way Mujoco renders the panda_pick_gym_env.py and shows when calling the test/test_gym_env_human.py.
    Need to add width and height & cameraid to the MujocoRenderer instantiation.

--- franka_sim/franka_sim/envs/panda_pick_gym_env.py
+++ franka_sim/franka_sim/envs/panda_pick_gym_env.py
@@ -144,6 +144,9 @@ class PandaPickCubeGymEnv(MujocoGymEnv):
         self._viewer = MujocoRenderer(
             self.model,
             self.data,
+            width=960,
+            height=960,
+            camera_id=0
         )
         self._viewer.render(self.render_mode)
 
