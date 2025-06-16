5bced50f7558b949064c97c8237986f1be3d0dd6
Juan Rojas <rojas70@gmail.com>
2025-06-16 15:14:55 -0500

    learner param settings changes: xla_python_client_mem_fraction and batch size

--- examples/async_peg_insert_drq/run_actor.sh
+++ examples/async_peg_insert_drq/run_actor.sh
@@ -1,12 +1,13 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
 python async_drq_randomized.py "$@" \
     --actor \
     --render \
     --env FrankaPegInsert-Vision-v0 \
     --exp_name=serl_dev_drq_rlpd10demos_peg_insert_random_resnet \
+    --max_steps 25000 \
     --seed 0 \
     --random_steps 0 \
     --training_starts 200 \
     --encoder_type resnet-pretrained \
-    --demo_path peg_insert_20_demos_2025-04-10_15-36-26.pkl \
+    --demo_path peg_insert_20_demos_2025-04-14_15-36-51.pkl \
--- examples/async_peg_insert_drq/run_learner.sh
+++ examples/async_peg_insert_drq/run_learner.sh
@@ -1,16 +1,17 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.2 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.6 && \
 python async_drq_randomized.py "$@" \
     --learner \
     --env FrankaPegInsert-Vision-v0 \
     --exp_name=serl_dev_drq_rlpd10demos_peg_insert_random_resnet_097 \
     --seed 0 \
+    --max_steps 25000 \
     --random_steps 1000 \
     --training_starts 200 \
     --critic_actor_ratio 4 \
-    --batch_size 256 \
+    --batch_size 128 \
     --eval_period 2000 \
     --encoder_type resnet-pretrained \
-    --demo_path peg_insert_20_demos_2025-04-10_15-36-26.pkl \
+    --demo_path peg_insert_20_demos_2025-04-14_15-36-51.pkl\
     --checkpoint_period 1000 \
     --checkpoint_path /home/student/robot/robot_ws/src/serl/examples/async_peg_insert_drq/checkpoints
