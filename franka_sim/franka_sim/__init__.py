b6877196c36243f4be54cc766fb894c95aba6932
Cleiver Ivan Ruiz-Martinez <CIRuizMartinez@mail.lipscomb.edu>
2025-07-04 17:26:28 -0500

    phase 1 of reach env, no V&V test yet

--- franka_sim/franka_sim/__init__.py
+++ franka_sim/franka_sim/__init__.py
@@ -18,3 +18,15 @@ register(
     max_episode_steps=100,
     kwargs={"image_obs": True},
 )
+register(
+    id="PandaReachCube-v0",
+    entry_point="franka_sim.envs:PandaReachCubeGymEnv",
+    max_episode_steps=100,
+)
+# register(
+#     id="PandaReachCubeVision-v0",
+#     entry_point="franka_sim.envs:PandaReachCubeGymEnv",
+#     max_episode_steps=100,
+#     kwargs={"image_obs": True},
+# )
+    
