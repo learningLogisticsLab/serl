bdac8733e14c5cadb4991e17d8cceaadcfeda999
ryanvanderstelt <ryan@vstelt.com>
2025-07-07 17:26:28 -0500

    Organization underway, not functional or tested

--- serl_launcher/serl_launcher/data/fractal_symmetry_replay_buffer.py
+++ serl_launcher/serl_launcher/data/fractal_symmetry_replay_buffer.py
@@ -9,5 +9,50 @@ from flax.core import frozen_dict
 from gym.spaces import Box
 
 class FractalSymmetryReplayBuffer(ReplayBuffer):
-    def __init__():
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        action_space: gym.Space,
+        capacity: int,
+        split_method: str,
+        split_freq: int, # For time-based only
+        split_qs: int,
+        branch_method: str,
+    ):
+        self.split_method=split_method
+        self.split_qs=split_qs
+        self.split_freq=split_freq
+        self.branch_method=branch_method
+        # TODO
+        #   Initialize passed-in variables as needed 
+        #   Add flags for variables passed to
+        #   Function definition dependent on flags?
+
+        super().__init__(
+            self,
+            observation_space=observation_space,
+            action_space=action_space,
+            capacity=capacity
+        )
+
+    # NOT TESTED
+    def transform(self, data_dict: DatasetDict, translation: np.array):
+        if not isinstance(data_dict, dict):
+            return
+        for k in data_dict.keys():
+            if (self.placeholder): # if key is included in list provided
+                data_dict[k] += translation
+            else:
+                self.transform(data_dict[k], translation)
+    
+    def fractal_branch(self, data_dict: DatasetDict):
+        return
+    
+    def constant_branch(self, data_dict: DatasetDict):
+        return
+    
+    def linear_branch(self, data_dict: DatasetDict):
+        return
+    
+    def insert(self, data_dict: DatasetDict):
         return
