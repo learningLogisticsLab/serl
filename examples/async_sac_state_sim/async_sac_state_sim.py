b3c77f264ce259819967e82c2b4cbb596309e365
ryanvanderstelt <ryan@vstelt.com>
2025-07-18 15:25:34 -0500

    Added y-dimension

--- examples/async_sac_state_sim/async_sac_state_sim.py
+++ examples/async_sac_state_sim/async_sac_state_sim.py
@@ -293,6 +293,7 @@ def main(_):
             split_method="test",
             workspace_width=0.5,
             x_obs_idx=np.array([0,7]),
+            y_obs_idx=np.array([1,8]),
             preload_rlds_path=FLAGS.preload_rlds_path,
         )
         replay_iterator = replay_buffer.get_iterator(
