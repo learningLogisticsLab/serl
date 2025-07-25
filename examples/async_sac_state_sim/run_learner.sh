5400702c5a18709403d4f03b2f5913ae4fafd52a
Juan Rojas <rojas70@gmail.com>
2025-07-25 10:52:55 -0500

    cleaning up test scripts as we have made advances there in fractal branch

--- examples/async_sac_state_sim/run_learner.sh
+++ examples/async_sac_state_sim/run_learner.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.5 && \
-export SCRIPT_DIR=$(dirname "$(realpath "$0")") && \
-export TIMESTAMP=$(date +"%m-%d-%Y-%H-%M-%S") && \
-export CHECKPOINT_DIR="/data/fsrb_testing/checkpoints-$TIMESTAMP" && \
-
-# Create checkpoint directory if it doesn't exist
-if [ ! -d "$CHECKPOINT_DIR" ]; then
-    echo "Creating checkpoint directory: $CHECKPOINT_DIR"
-    mkdir -p "$CHECKPOINT_DIR" || {
-        echo "Failed to create checkpoint directory!" >&2
-        exit 1
-    }
-fi
 
 python async_sac_state_sim.py \
     --learner \
