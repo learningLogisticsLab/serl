e0e06333232b0aeaaa11e21504e73fdbdc2c1aa7
Juan Rojas <rojas70@gmail.com>
2025-07-30 14:16:11 -0500

    update sh files to work with fractal_symmetry_replay_buffer

--- examples/async_sac_state_sim/run_actor.sh
+++ examples/async_sac_state_sim/run_actor.sh
@@ -6,18 +6,18 @@ export TIMESTAMP=$(date +"%m-%d-%Y-%H-%M-%S") && \
 export CHECKPOINT_DIR="/data/fsrb_testing/checkpoints-$TIMESTAMP" && \
 
 # Create checkpoint directory if it doesn't exist
-if [ ! -d "$CHECKPOINT_DIR" ]; then
-    echo "Creating checkpoint directory: $CHECKPOINT_DIR"
-    mkdir -p "$CHECKPOINT_DIR" || {
-        echo "Failed to create checkpoint directory!" >&2
-        exit 1
-    }
-fi
+# if [ ! -d "$CHECKPOINT_DIR" ]; then
+#     echo "Creating checkpoint directory: $CHECKPOINT_DIR"
+#     mkdir -p "$CHECKPOINT_DIR" || {
+#         echo "Failed to create checkpoint directory!" >&2
+#         exit 1
+#     }
+# fi
 
 python async_sac_state_sim.py \
     --actor \
     --env PandaReachCube-v0 \
-    --exp_name PandaReachCube-v0_state_sim_3D_con-81-3-batch_2048_replay_8M_utd_32 \
+    --exp_name PandaReachCube-v0_state_sim_demos_27^1_batch_2048_replay_8M_utd_32 \
     --seed 0 \
     --replay_buffer_type fractal_symmetry_replay_buffer \
     --max_steps 300_000 \
@@ -26,21 +26,19 @@ python async_sac_state_sim.py \
     --batch_size 2048 \
     --replay_buffer_capacity 8_000_000 \
     --save_model True \
-    # --load_demos \
-    # --demo_dir /data/data/serl/demos \
-    # --file_name data_franka_reach_random_5_2.npz \  
-    --branch_method contraction \
-    --split_method time \
-    --max_traj_length 100 \
+    --load_demos \
+    --demo_dir /data/data/serl/demos \
+    --file_name data_franka_reach_random_5_2.npz \  
+    --branch_method constant \
+    --split_method constant \
+    # --max_traj_length 100 \
     --max_depth 4 \
-    --start_num 81 \
+    # --start_num 81 \
     --alpha 1 \
-    --max_depth 4 \
+    # --max_depth 4 \
     --branching_factor 3 \
     --workspace_width 0.5 \
-    --render
     # --checkpoint_period 10000 \
     # --checkpoint_path "$CHECKPOINT_DIR" \
     #--render \
     #--debug # wandb is disabled when debug
-    #--render 
--- examples/async_sac_state_sim/run_learner.sh
+++ examples/async_sac_state_sim/run_learner.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.2 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.5 && \
+export SCRIPT_DIR=$(dirname "$(realpath "$0")") && \
+export TIMESTAMP=$(date +"%m-%d-%Y-%H-%M-%S") && \
+export CHECKPOINT_DIR="/data/fsrb_testing/checkpoints-$TIMESTAMP" && \
+
+# Create checkpoint directory if it doesn't exist
+# if [ ! -d "$CHECKPOINT_DIR" ]; then
+#     echo "Creating checkpoint directory: $CHECKPOINT_DIR"
+#     mkdir -p "$CHECKPOINT_DIR" || {
+#         echo "Failed to create checkpoint directory!" >&2
+#         exit 1
+#     }
+# fi
 
 python async_sac_state_sim.py "$@"\
     --learner \
     --env PandaReachCube-v0 \
-    --exp_name PandaReachCube-v0_async_sac_state_demos_5_replay_1M_batch_2048_utd_32 \
+    --exp_name PandaReachCube-v0_state_sim_demos_27^1_batch_2048_replay_8M_utd_32 \
     --replay_buffer_type replay_buffer \
     --max_steps 50_000 \
     --training_starts 1000 \
     --random_steps 1000 \
     --critic_actor_ratio 32 \
     --batch_size 2048 \
-    --replay_buffer_capacity 1_000_000 \
+    --replay_buffer_capacity 8_000_000 \
     --save_model True \
-    # --load_demos \
-    # --demo_dir /data/data/serl/demos \
-    # --file_name data_franka_reach_random_5_2.npz \    
-    --branch_method contraction \
-    --split_method time \
-    --starting_branch_count 1 \
-    --max_traj_length 100 \
-    --start_num 81 \
-    --alpha 1 \
-    --max_depth 4 \
-    --branching_factor 3 \
+    --load_demos \
+    --demo_dir /data/data/serl/demos \
+    --file_name data_franka_reach_random_5_2.npz \    
+    --branch_method constant \
+    --split_method constant \
     --workspace_width 0.5 \
+    --starting_branch_count 1 \
+    # --max_traj_length 100 \
+    # --start_num 81 \
+    # --alpha 1 \
+    # --max_depth 4 \
+    # --branching_factor 3 \
     # --checkpoint_period 10000 \
     # --checkpoint_path "$CHECKPOINT_DIR" \
     #--debug # wandb is disabled when debug
