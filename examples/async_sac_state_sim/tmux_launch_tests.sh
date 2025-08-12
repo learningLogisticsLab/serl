ee086184738c864380a11b81a791961eab49dd36
ryanvanderstelt <ryan@vstelt.com>
2025-08-12 09:47:14 -0500

    improved test files

--- examples/async_sac_state_sim/tmux_launch_tests.sh
+++ examples/async_sac_state_sim/tmux_launch_tests.sh
@@ -13,17 +13,11 @@ do
     tmux split-window -v
     tmux split-pane -h -t serl_session:$SEED.1
 
-    # Find open ports for TrainerServer
-    OPEN_PORTS=$( comm -23 <(seq 49152 65535 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 2 )
-    PORTS=( $OPEN_PORTS )
     # Navigate to the activate the conda environment in the first pane
-    tmux send-keys -t serl_session:$SEED.0 "bash automated_tests.sh $SEED ${PORTS[0]} ${PORTS[1]}" C-m
-    echo ${PORTS[0]}
-    echo ${PORTS[1]}
+    tmux send-keys -t serl_session:$SEED.0 "bash automated_tests.sh $SEED" C-m
 
 done
 # Attach to the tmux session
-tmux kill-window -t serl_sessoin:0
 tmux attach-session -t serl_session
 
 # kill the tmux session by running the following command
